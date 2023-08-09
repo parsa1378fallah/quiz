4b0858938dd21df5bacb904462456ab782bae03d
parsa <parsaddbb@gmail.com>
2023-08-09 08:23:49 +0330

    add changes

--- vite.config.js
+++ vite.config.js
@@ -7,9 +7,4 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   base : '/parsa1378fallah/',
   plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
 })
